e041b15f9cbdb8e53b305a10d9fe6b6a69f543bd
Akos Balogh <hello.akosb@gmail.com>
2020-02-15 22:03:33 +0100

    Add API's Job type

---
+++ src/types.d.ts
@@ -0,0 +1,25 @@
+//demo json:
+// {
+//     "id": "d25d1423-54ba-494b-a9e1-9b44055c6c8f",
+//     "type": "....",
+//     "url": "...",
+//     "created_at": "Thu Feb 13 22:45:33 UTC 2020",
+//     "company": "...",
+//     "company_url": "...",
+//     "location": "...",
+//     "title": "...",
+//     "description": "...",
+//     "company_logo": "..."
+// }
+export type Job ={
+    "id": string;
+    "type": string;
+    "url": string;
+    "created_at": string;
+    "company": string;
+    "company_url": string;
+    "location": string;
+    "title": string;
+    "description": string;
+    "company_logo": string;
+}
